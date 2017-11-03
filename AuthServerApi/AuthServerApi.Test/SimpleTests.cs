b03f5421916ce79ee5f1505652e271db6132e508
Aaron Gibson <Aaron.Gibson@Cascadehr.co.uk>
2017-11-03 15:29:01 +0000

    unit test example

---
+++ AuthServerApi/AuthServerApi.Test/SimpleTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace AuthServerApi.Test
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void DumyTest()
+        {
+            var result = false;
+
+            Assert.AreEqual(true, false);
+        }
+    }
+}
